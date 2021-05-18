5c59da613231a28f3ac9f896e409695c80650a06
JonathanZYZ <jonathanzengyuzhao@gmail.com>
2021-05-18 14:32:42 +0800

    2nd activity JClass

--- app/src/main/java/sg/edu/ro/c346/id16046530/p04problemstatement/SecondActivity.java
+++ app/src/main/java/sg/edu/ro/c346/id16046530/p04problemstatement/SecondActivity.java
@@ -1,6 +1,9 @@
 package sg.edu.ro.c346.id16046530.p04problemstatement;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -12,6 +15,7 @@ public class SecondActivity extends AppCompatActivity {
 
     ListView lv;
     CustomAdapter adapter;
+    ArrayList<Song> al;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +29,14 @@ public class SecondActivity extends AppCompatActivity {
 
         adapter = new CustomAdapter(this, R.layout.second_row, Songs);
         lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+                intent.putExtra("Code", al.get(position).getId());
+                startActivityForResult(intent, 9);
+            }
+        });
     }
 }
