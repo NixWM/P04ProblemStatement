f583b53a389069ce954c80121b8558c65bd8514d
JonathanZYZ <jonathanzengyuzhao@gmail.com>
2021-05-18 15:07:04 +0800

    working on 5 star only list and sipnner

--- app/src/main/java/sg/edu/ro/c346/id16046530/p04problemstatement/SecondActivity.java
+++ app/src/main/java/sg/edu/ro/c346/id16046530/p04problemstatement/SecondActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +17,7 @@ public class SecondActivity extends AppCompatActivity {
     ListView lv;
     CustomAdapter adapter;
     ArrayList<Song> al;
+    Button btn5star;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,7 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.second_activity);
 
         lv = (ListView) this.findViewById(R.id.lv);
+        btn5star = findViewById(R.id.btn5star);
 
         DBHelper dbh = new DBHelper(this);
         ArrayList<Song> Songs = dbh.getAllNotes();
@@ -38,5 +41,16 @@ public class SecondActivity extends AppCompatActivity {
                 startActivityForResult(intent, 9);
             }
         });
+
+        btn5star.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<Song> Songs = dbh.getAllNotes();
+                adapter = new CustomAdapter(SecondActivity.this, R.layout.second_row, Songs);
+                lv.setAdapter(adapter);
+            }
+        });
+
+
     }
 }
