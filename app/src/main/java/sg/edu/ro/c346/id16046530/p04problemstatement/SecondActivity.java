55824a510e6d0a9d3297c66a9b2b2c1b6fe39018
JonathanZYZ <jonathanzengyuzhao@gmail.com>
2021-05-18 13:21:20 +0800

    activity 2 XML and JClass

--- app/src/main/java/sg/edu/ro/c346/id16046530/p04problemstatement/SecondActivity.java
+++ app/src/main/java/sg/edu/ro/c346/id16046530/p04problemstatement/SecondActivity.java
@@ -1,4 +1,28 @@
 package sg.edu.ro.c346.id16046530.p04problemstatement;
 
-public class SecondActivity {
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class SecondActivity extends AppCompatActivity {
+
+    ListView lv;
+    ArrayAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+
+        lv = (ListView) this.findViewById(R.id.lv);
+
+        DBHelper dbh = new DBHelper(this);
+        ArrayList<Note> notes = dbh.getAllNotes();
+
+        adapter = new ArrayAdapter(this, R.layout.row, notes);
+        lv.setAdapter(adapter);
+    }
 }
