8b877fdbc1a348f14ee74fc77264c7b17ac62b29
JonathanZYZ <jonathanzengyuzhao@gmail.com>
2021-05-18 14:13:36 +0800

    activity 2 XML and JClass edited

--- app/src/main/java/sg/edu/ro/c346/id16046530/p04problemstatement/SecondActivity.java
+++ app/src/main/java/sg/edu/ro/c346/id16046530/p04problemstatement/SecondActivity.java
@@ -21,9 +21,9 @@ public class SecondActivity extends AppCompatActivity {
         lv = (ListView) this.findViewById(R.id.lv);
 
         DBHelper dbh = new DBHelper(this);
-        //ArrayList<Note> notes = dbh.getAllNotes();
+        ArrayList<Song> notes = dbh.getAllNotes();
 
-        //adapter = new ArrayAdapter(this, R.layout.row, notes);
+        adapter = new ArrayAdapter(this, R.layout.second_row, notes);
         lv.setAdapter(adapter);
     }
 }
